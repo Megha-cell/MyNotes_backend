46df1ba3fed45d4ae03a8dce58f5b9fe3f5738f8
Megha-cell <meghakamlekar123@gmail.com>
2025-05-06 00:34:45 +0530

    few changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import connectToMongo from "./config/db.js";
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";
-import noteRoutes from "./routes/notes.js";
+import auth from "./routes/auth.js";
+import notes from "./routes/notes.js";
 dotenv.config();
 
 connectToMongo(); // Establish MongoDB connection
@@ -28,8 +28,8 @@ app.use(
 //app.use(cors({ origin: 'https://i-notebook-one-delta.vercel.app' }))
 app.use(express.json()); //t be able to use req.body
 
-app.use("/api/auth", authRoutes);
-app.use("/api/notes", noteRoutes);
+app.use("/api/auth", auth);
+app.use("/api/notes", notes);
 
 app.listen(port, () => {
   console.log(`MyNotes backend listening at ${port}`);
