f161edf79c89872e9112132720ae7b76f5057eaa
Megha-cell <meghakamlekar123@gmail.com>
2025-05-06 22:46:57 +0530

    few changes12

--- routes/auth.js
+++ routes/auth.js
@@ -107,7 +107,7 @@ router.post(
 //ROUTE:3:Get loggedin User details using: POST "api/auth/getuser" login required
 router.post("/getuser", fetchUser, async (req, res) => {
   try {
-    userId = req.user.id;
+    const userId = req.user.id;
     const user = await User.findById(userId).select("-password");
     res.send(user);
   } catch (error) {
